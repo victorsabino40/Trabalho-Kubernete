d19de93fb13061a5f2e6edfd062a0be5868f85d6
victorsabino40 <56826518+victorsabino40@users.noreply.github.com>
2025-11-12 16:13:05 -0300

    Delete HealthTrack_CORRIGIDO directory

--- HealthTrack_CORRIGIDO/HealthTrack_CORRIGIDO/kubernetes/scripts/portforward.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-kubectl -n frontend port-forward svc/frontend-svc 8080:80
-# Em outro terminal, se desejar:
-# kubectl -n backend port-forward svc/backend-svc 3000:3000
--- HealthTrack_CORRIGIDO/kubernetes/scripts/tests.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-curl -sSf http://localhost:30080 >/dev/null && echo "OK frontend (200)"
-curl -sSf http://localhost:3000/pacientes >/dev/null && echo "OK backend /pacientes"
-curl -sSf http://localhost:3000/consultas >/dev/null && echo "OK backend /consultas"
